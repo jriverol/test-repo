954288697150e758a264e8a68e1ac68ba9c800be
jriverol <joseluis.rivero.laguna@gmail.com>
2023-08-02 12:40:07 +0200

    first commit

---
+++ src/main/java/com/ejemplo/App.java
@@ -0,0 +1,23 @@
+package com.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class SaludoController {
+
+    @GetMapping("/saludo")
+    public String obtenerSaludo() {
+        return "¡Hola, este es un saludo desde Spring Boot!";
+    }
+}
